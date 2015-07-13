d56ea27377aa79cd0bd672c870a05805f8fca42b
pritesh.patel <lssmessy@gmail.com>
2015-07-13 19:47:04 +0530

    Ownername with date is working

--- Mansi_Flowers/Add_Lilies.Designer.cs
+++ Mansi_Flowers/Add_Lilies.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add_Lilies));
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button3 = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,12 +56,12 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 37);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(333, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(415, 301);
             this.dataGridView1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(351, 37);
+            this.button1.Location = new System.Drawing.Point(433, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 62);
             this.button1.TabIndex = 2;
@@ -68,7 +72,7 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(351, 105);
+            this.button2.Location = new System.Drawing.Point(433, 105);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 62);
             this.button2.TabIndex = 3;
@@ -80,7 +84,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(34, 199);
+            this.label1.Location = new System.Drawing.Point(39, 341);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 4;
@@ -89,18 +93,43 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 199);
+            this.label2.Location = new System.Drawing.Point(119, 341);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "label2";
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(433, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 39);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "P&rint";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
             // 
             // Add_Lilies
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button2;
-            this.ClientSize = new System.Drawing.Size(488, 230);
+            this.ClientSize = new System.Drawing.Size(570, 372);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -124,5 +153,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
--- Mansi_Flowers/Add_Lilies.cs
+++ Mansi_Flowers/Add_Lilies.cs
@@ -17,6 +17,8 @@ namespace Mansi_Flowers
         private OleDbCommand cmd = new OleDbCommand();
         private String connectionString = Global_Connection.conn;
         private DataTable dtbl;
+        List<String> ownernames = new List<string>();
+        List<String> lili = new List<string>();
         public Add_Lilies()
         {
             conn = new OleDbConnection(connectionString);
@@ -31,76 +33,227 @@ namespace Mansi_Flowers
         private void Add_Lilies_Load(object sender, EventArgs e)
         {
             string theDate = dateTimePicker1.Value.ToString("dd-MM-yyyy");
-            dtbl = new DataTable();
-            dtbl.Columns.Add("Owner_ID");
-            dtbl.Columns.Add("OwnerName");
-            dtbl.Columns.Add("Lilies");
-            dataGridView1.DataSource = dtbl;
-            //dataGridView1.Rows.Clear();
-            dataGridView1.Refresh();
-            String query = "SELECT Owner_ID,OwnerName,Lilies FROM lilie_master";
-            OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
-            OleDbCommandBuilder cmdBuilder = new OleDbCommandBuilder(adapter);
-            DataTable tbl = new DataTable();
-            DataSet ds = new DataSet();
-            adapter.Fill(dtbl);
-            for (int i = 0; i < tbl.Rows.Count; i++) {
-                dataGridView1.Rows.Add(dtbl.Rows[i][0], dtbl.Rows[i][1],dtbl.Rows[i][2]);
+            try {
+                
+                cmd.Connection = conn;
+                conn.Open();
+                String query = "SELECT Owner_ID,OwnerName FROM owner_master";
+                OleDbDataAdapter adp = new OleDbDataAdapter(query, conn);
+                //OleDbCommandBuilder builder = new OleDbCommandBuilder(adp);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+
+                
+                for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+                {
+                    
+                    cmd.CommandText = ("SELECT COUNT(*) FROM lilie_master WHERE Owner_ID=" + ds.Tables[0].Rows[i].ItemArray[0].ToString() + "AND Lilie_Date LIKE '%" + theDate + "%'");
+                    int cnt = (int)cmd.ExecuteScalar();
+                    if (cnt != 1 && cnt==0) {
+                    
+                    cmd.CommandText = ("INSERT INTO lilie_master(Owner_ID,Lilie_Date,OwnerName) VALUES(" + ds.Tables[0].Rows[i].ItemArray[0].ToString() + ",'" + theDate + "','" + ds.Tables[0].Rows[i].ItemArray[1].ToString() + "')");
+                    cmd.ExecuteNonQuery();
+                    }
+
+                 
+                }
+                conn.Close();
+                //MessageBox.Show("inserted");
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+
+            try
+            {
+                dtbl = new DataTable();
+                dtbl.Columns.Add("Owner_ID");
+                dtbl.Columns.Add("OwnerName");
+                dtbl.Columns.Add("Lilies");
+                dtbl.Columns["Owner_ID"].ReadOnly = true;
+                dtbl.Columns["OwnerName"].ReadOnly = true;
+                dataGridView1.DataSource = dtbl;
+                dataGridView1.Refresh();
+                String query1 = "SELECT Owner_ID,OwnerName,Lilies FROM lilie_master WHERE Lilie_Date='" + theDate + "'";
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query1, conn);
+                OleDbCommandBuilder cmdBuilder = new OleDbCommandBuilder(adapter);
+                DataTable tbl = new DataTable();
+
+                adapter.Fill(dtbl);
+                for (int i = 0; i < tbl.Rows.Count; i++)
+                {
+
+                    dataGridView1.Rows.Add(dtbl.Rows[i][0], dtbl.Rows[i][1], dtbl.Rows[i][2]);
 
 
+                }
+            }
+            catch (Exception exp) {
+                MessageBox.Show(exp.ToString());
             }
             
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            
-            int count=dataGridView1.Rows.Count;
-            dataGridView1.Rows[0].Cells[1].Value.GetType();
-            string theDate = dateTimePicker1.Value.ToString("dd-MM-yyyy");
-            for (int i = 0; i < count; i++)
-            {
-                int oid = Int32.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
-                String oname = dataGridView1.Rows[i].Cells[1].Value.ToString();
-                
 
-                cmd.Connection = conn;
-                conn.Open();
-                cmd.CommandText = ("INSERT INTO lilie_master(Lilies,Lili_Date,Owner_ID) VALUES(" + dataGridView1.Rows[i].Cells[2].Value + ", " + theDate + ", " + oid + ")");
-                cmd.ExecuteNonQuery();
-                conn.Close();
-            }
-            MessageBox.Show("Data updated");
+            //try
+            //{
+                int count = dataGridView1.Rows.Count;
+                string theDate = dateTimePicker1.Value.ToString("dd-MM-yyyy");
+                for (int i = 0; i < count; i++)
+                {
+                    //int oid = Int32.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    //String oname = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    //int lilis = (int)dataGridView1.Rows[i].Cells[2].Value;
+                    Random rm = new Random(100);
+                    cmd.Connection = conn;
+                    conn.Open();
+                    cmd.CommandText = ("UPDATE lilie_master SET Lilies='" + dataGridView1.Rows[i].Cells[2].Value + "' WHERE Owner_ID=" + dataGridView1.Rows[i].Cells[0].Value + " AND Lilie_Date =" + theDate + "");//(Lilie_Date ='" + theDate + "' AND 
+                    cmd.ExecuteNonQuery();
+                    //int s = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                }
+                MessageBox.Show("Data updated");
+            //}
+            //catch (Exception exp) {
+            //    MessageBox.Show(exp.ToString());
+            //}
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
             string theDate = dateTimePicker1.Value.ToString("dd-MM-yyyy");
-            dtbl = new DataTable();
-            dtbl.Columns.Add("Owner_ID");
-            dtbl.Columns.Add("OwnerName");
-            dtbl.Columns.Add("Lilies");
-            dataGridView1.DataSource = dtbl;
-            //dataGridView1.Rows.Clear();
-            dataGridView1.Refresh();
-            String query = "SELECT Owner_ID,OwnerName FROM owner_master";//,Lilies FROM lilie_master WHERE Lilie_Date=" + theDate + " ";
-            OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
-            OleDbCommandBuilder cmdBuilder = new OleDbCommandBuilder(adapter);
-            DataTable tbl = new DataTable();
-            DataSet ds = new DataSet();
-            
-            adapter.Fill(dtbl);
-            
-            for (int i = 0; i < tbl.Rows.Count; i++)
+            try
             {
-                if (adapter == null) {
-                    dtbl.Rows[i][2] = "";
-                }
-                dataGridView1.Rows.Add(dtbl.Rows[i][0], dtbl.Rows[i][1], dtbl.Rows[i][2]);
+               
+                cmd.Connection = conn;
+                conn.Open();
+                String query = "SELECT Owner_ID,OwnerName FROM owner_master";
+                OleDbDataAdapter adp = new OleDbDataAdapter(query, conn);
+                //OleDbCommandBuilder builder = new OleDbCommandBuilder(adp);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
 
+
+                for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+                {
+                    cmd.CommandText = ("SELECT COUNT(*) FROM lilie_master WHERE Owner_ID=" + ds.Tables[0].Rows[i].ItemArray[0].ToString() + "AND Lilie_Date='" + theDate + "'");//
+                    int cnt = (int)cmd.ExecuteScalar();
+                    if (cnt != 1 & cnt == 0)
+                    {
+            
+                        cmd.CommandText = ("INSERT INTO lilie_master(Owner_ID,Lilie_Date,OwnerName) VALUES(" + ds.Tables[0].Rows[i].ItemArray[0].ToString() + ",'" + theDate + "','" + ds.Tables[0].Rows[i].ItemArray[1].ToString() + "')");
+                        cmd.ExecuteNonQuery();
+                        
+                    }
+
+
+                }
+                conn.Close();
+                //MessageBox.Show("After that");
+
+                
 
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            try
+            {
+                dtbl = new DataTable();
+                dtbl.Columns.Add("Owner_ID");
+                dtbl.Columns.Add("OwnerName");
+                dtbl.Columns.Add("Lilies");
+                dtbl.Columns["Owner_ID"].ReadOnly = true;
+                dtbl.Columns["OwnerName"].ReadOnly = true;
+                dataGridView1.DataSource = dtbl;
+                dataGridView1.Refresh();
+                String query1 = "SELECT Owner_ID,OwnerName,Lilies FROM lilie_master WHERE Lilie_Date='" + theDate + "'";
+                OleDbDataAdapter adapter = new OleDbDataAdapter(query1, conn);
+                OleDbCommandBuilder cmdBuilder = new OleDbCommandBuilder(adapter);
+                DataTable tbl = new DataTable();
+
+                adapter.Fill(dtbl);
+                for (int i = 0; i < tbl.Rows.Count; i++)
+                {
+
+                    dataGridView1.Rows.Add(dtbl.Rows[i][0], dtbl.Rows[i][1], dtbl.Rows[i][2]);
+
+
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+            
+
+            //try
+            //{
+            //    string theDate = dateTimePicker1.Value.ToString("dd-MM-yyyy");
+            //    dtbl = new DataTable();
+            //    dtbl.Columns.Add("Owner_ID");
+            //    dtbl.Columns.Add("OwnerName");
+            //    dtbl.Columns.Add("Lilies");
+            //    dataGridView1.DataSource = dtbl;
+            //    dataGridView1.Refresh();
+            //    String query = "SELECT Owner_ID,OwnerName FROM lilie_master ";//WHERE Lilie_Date LIKE '%" + theDate + "%'";
+
+            //    OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+            //    OleDbCommandBuilder cmdBuilder = new OleDbCommandBuilder(adapter);
+            //    DataTable tbl = new DataTable();
+
+            //    cmd.Connection = conn;
+            //    conn.Open();
+            //    cmd.CommandText = "SELECT Lilies FROM lilie_master WHERE Lilie_Date=" + theDate + " ";
+            //    OleDbDataReader rd = cmd.ExecuteReader();
+            //    while (rd.Read())
+            //    {
+            //        lili.Add(rd[0].ToString());
+            //    }
+
+            //    conn.Close();
+            //    //for lilies
+
+            //    DataSet ds = new DataSet();
+
+            //    adapter.Fill(dtbl);
+
+            //    for (int i = 0; i < tbl.Rows.Count; i++)
+            //    {
+            //        dtbl.Rows[i][2] = lili[i];
+            //        dataGridView1.Rows.Add(dtbl.Rows[i][0], dtbl.Rows[i][1], dtbl.Rows[i][2]);
+            //        //cmd.Connection = conn;
+            //        //conn.Open();
+            //        //cmd.CommandText = ("INSERT INTO lilie_master(Lilie_Date,Owner_ID) VALUES('" + theDate + "'," + dtbl.Rows[i][0] + ")");
+            //        //cmd.ExecuteNonQuery();
+            //        //conn.Close();
+
+            //    }
+            //}
+            //catch (Exception ex) {
+            //    MessageBox.Show(ex.ToString());
+                
+            //}
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bm, new Rectangle(0,0,this.dataGridView1.Width, this.dataGridView1.Height));
+            e.Graphics.DrawImage(bm, 0, 0);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+            
         }
     }
 }
